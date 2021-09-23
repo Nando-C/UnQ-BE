c18af51f625637644e2d23ad851f7c18efcc2b30
Nando-C <73954373+Nando-C@users.noreply.github.com>
2021-09-23 18:58:28 +0100

    adds Users schema, model & types

---
+++ src/users/model.ts
@@ -0,0 +1,57 @@
+import mongoose from "mongoose"
+import { IUserDocument, IUserModel } from "src/typings/users"
+import bcrypt from "bcrypt"
+
+const { Schema, model } = mongoose
+
+const UserSchema = new Schema<IUserDocument,IUserModel>(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        surname: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        avatar: String,
+        role: {
+            type: String,
+            enum: ["Customer", "StoreMg"],
+            default: "Customer"
+        },
+        refreshToken: String,
+    },
+    { timestamps: true }
+)
+
+UserSchema.pre("save", async function (next) {
+    if (this.isModified("password")) this.password = await bcrypt.hash(this.password!, 10)
+    next()
+})
+
+UserSchema.statics.checkCredentials = async function (email, password) {
+    const user = await this.findOne({email})
+    if (!user) return
+    const isMatch = await bcrypt.compare(password, user.password!)
+    if (isMatch) return user
+}
+
+UserSchema.methods.toJSON = function () {
+    const user = this.toObject()
+    delete user.__v
+    delete user.refreshToken
+    delete user.password
+    return user
+}
+
+export default model<IUserDocument, IUserModel>("User", UserSchema)
